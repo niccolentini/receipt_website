01affc7e5a68555ce49036d53057ac04e1f490a2
niccolentini <niccolo.lentini@stud.unifi.it>
2023-06-02 16:54:51 +0200

    try to deploy again removing al links from requirements, ora ho cambiato cose in database

--- receipt_website/settings.py
+++ receipt_website/settings.py
@@ -88,15 +88,15 @@ WSGI_APPLICATION = 'receipt_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=600)
+#DATABASES['default'].update(db_from_env)
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
