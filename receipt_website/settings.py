a4693eab851bb76a2f828a00751e99649eb1ac6c
niccolentini <niccolo.lentini@stud.unifi.it>
2023-06-07 15:00:25 +0200

    added cors origin whitelist

--- receipt_website/settings.py
+++ receipt_website/settings.py
@@ -28,10 +28,12 @@ SECRET_KEY = 'django-insecure-ku^relw#kytd)7%8!7o(8p!b%p+pd7b@reuab!z07=7)dz6b18
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://web-production-4d50.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://web-production-4d50.up.railway.app']
 CORS_ALLOWED_ORIGINS = ['https://web-production-4d50.up.railway.app']
 CSRF_COOKIE_DOMAIN = 'https://web-production-4d50.up.railway.app'
+CORS_ORIGIN_WHITELIST = ['https://web-production-4d50.up.railway.app']
+
 
 
 
